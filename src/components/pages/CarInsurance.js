df9280279eb2e5e059b9399040d7f56005e28c41
Aydin Battal <abattal35@gmail.com>
2021-05-08 15:01:38 -0400

    Added categories for car insurance claim.

--- src/components/pages/CarInsurance.js
+++ src/components/pages/CarInsurance.js
@@ -4,6 +4,22 @@ function CarInsurance() {
   return (
     <div className="carinsurance">
       <h1>Car Insurance</h1>
+      <div>
+        <img />
+        <h2>Collision</h2>
+      </div>
+      <div>
+        <img />
+        <h2>Hail</h2>
+      </div>
+      <div>
+        <img />
+        <h2>Hit and run</h2>
+      </div>
+      <div>
+        <img />
+        <h2>Theft</h2>
+      </div>
     </div>
   );
 }
