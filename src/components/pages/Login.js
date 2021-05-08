82dd8a30a5611217b4deb43a1a901c0195cee5e6
Adnan Joraid <adnanjoraidca@gmail.com>
2021-05-08 18:24:49 -0400

    working

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -1,40 +1,32 @@
-import React, { useRef } from "react";
-import { auth } from "../../firebase";
+import React, { useRef } from 'react'
+import { auth } from '../../Firebase';
 
 const Signin = () => {
-  const emailRef = useRef(null);
-  const passwordRef = useRef(null);
+    const emailRef = useRef(null);
+    const passwordRef = useRef(null);
+  
+    const signIn = e => {
+        e.preventDefault();
+        auth.signInWithEmailAndPassword(
+            emailRef.current.value,
+            passwordRef.current.value
+        ).then(user => {
+            console.log(user)
+        }).catch(err => {
+            console.log(err)
+        })
+    }
+    return (
+        <div className="signin">
+            <form action="">
+                <h1>Sign in</h1>
+                <input ref={emailRef} type="email" />
+                <input ref={passwordRef} type="password" />
+                <button onClick={signIn}>Sign in </button>
+                <h6>Not yet register? <span onClick={""} className="signin__link">Sign up</span></h6>
+            </form>
+        </div>
+    )
+}
 
-  const signIn = (e) => {
-    e.preventDefault();
-    auth
-      .signInWithEmailAndPassword(
-        emailRef.current.value,
-        passwordRef.current.value
-      )
-      .then((user) => {
-        console.log(user);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  };
-  return (
-    <div className="signin">
-      <form action="">
-        <h1>Sign in</h1>
-        <input ref={emailRef} type="email" />
-        <input ref={passwordRef} type="password" />
-        <button onClick={signIn}>Sign in </button>
-        <h6>
-          Not yet register?{" "}
-          <span onClick={""} className="signin__link">
-            Sign up
-          </span>
-        </h6>
-      </form>
-    </div>
-  );
-};
-
-export default Signin;
+export default Signin
