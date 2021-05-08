1a71e121174394e961cbc6365c9968cfab391025
Adnan Joraid <adnanjoraidca@gmail.com>
2021-05-08 16:26:39 -0400

    refactoring login

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -1,11 +1,16 @@
 import React from 'react'
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { auth } from '../../Firebase';
 
 function Login(){
-    return( 
-        <div>
-            <h1>Login</h1>
 
-        </div>
+   
+return( 
+    <div>
+        
+
+    </div>
     );
 }
 
