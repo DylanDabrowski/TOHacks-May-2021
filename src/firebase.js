44f9a597b9d175dda1ecbfdd841080c237a68154
Adnan Joraid <adnanjoraidca@gmail.com>
2021-05-08 16:27:02 -0400

    adding auth and firebase files that will handle authroization and authentication in addition to database

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,14 @@
-import firebase from 'firebase/app'
-import "firebase/auth"
-import "firebase/firestore";
+import * as firebase from "firebase/app";
+import "firebase/auth";
 
 
-const firebaseConfig = {
+const firebaseConfig = ({
     apiKey: "AIzaSyCBxn7tcZgwyMDIMhjBztJ_poU0RpUMMqo",
     authDomain: "tohacks-may-2021.firebaseapp.com",
     projectId: "tohacks-may-2021",
     storageBucket: "tohacks-may-2021.appspot.com",
     messagingSenderId: "476407277366",
     appId: "1:476407277366:web:0a12afd8ccbd1686f5b597"
-  };
-  // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-export const auth = firebase.auth();
-export const firestore = firebase.firestore();
+  });
+
+  export default firebaseConfig;
