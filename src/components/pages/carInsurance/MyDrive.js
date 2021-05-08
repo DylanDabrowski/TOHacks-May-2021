419ef3ce972641d13979bfb135c8ec1fc9eb0a6d
Adnan Joraid <adnanjoraidca@gmail.com>
2021-05-08 19:50:59 -0400

    added image to assets

--- src/components/pages/carInsurance/MyDrive.js
+++ src/components/pages/carInsurance/MyDrive.js
@@ -1,12 +1,26 @@
 import React from "react";
 import "./CarInsurance.css";
+import "./MyDrive.css";
 import SideNav from "../../CarSideNav.js";
+import mapImage from "./../../../assets/map.png";
+import graphImage from  "./../../../assets/graph.png";
 
 function MyDrive() {
   return (
     <div>
-      <SideNav />
+      <SideNav className="navBar"/>
+
+      <div className="background">
+        
+        
+
+      </div>
+      <h1 style={{textAlign: "left", top: 53, left: 551,}}>Score</h1>
+      <img className="image" src={mapImage} alt="location"/>
+      <img className="image" src={graphImage} alt="graph"/>
     </div>
+    
+    
   );
 }
 
