1a6d3b13e5685648b1592d9bca52ce4cb19ac3ea
Adnan Joraid <adnanjoraidca@gmail.com>
2021-05-08 17:22:02 -0400

    edited signIn functionalities

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -1,17 +1,33 @@
-import React from 'react'
-import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
-import { auth } from '../../Firebase';
+import React, { useRef } from 'react'
+import { auth } from './../../Firebase';
 
-function Login(){
 
-   
-return( 
-    <div>
-        
+const Signin = () => {
+    const emailRef = useRef(null);
+    const passwordRef = useRef(null);
 
-    </div>
-    );
+    const signIn = e => {
+        e.preventDefault();
+        auth.signInWithEmailAndPassword(
+            emailRef.current.value,
+            passwordRef.current.value
+        ).then(user => {
+            console.log(user)
+        }).catch(err => {
+            console.log(err)
+        })
+    }
+    return (
+        <div className="signin">
+            <form action="">
+                <h1>Sign in</h1>
+                <input ref={emailRef} type="email" />
+                <input ref={passwordRef} type="password" />
+                <button onClick={signIn}>Sign in </button>
+                <h6>Not yet register? <span className="signin__link">Sign up</span></h6>
+            </form>
+        </div>
+    )
 }
 
-export default Login;
+export default Signin
