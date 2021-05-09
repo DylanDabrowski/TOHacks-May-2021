620dfdabaad90ea2188315e6817de47bba7be4ec
Dylan Dabrowski <dabrowsd@sheridancollege.ca>
2021-05-09 00:14:15 -0400

    did some things

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -1,5 +1,5 @@
 import React, { useRef } from "react";
-import { auth } from "../../firebase";
+import { auth } from "../../Firebase";
 import "./Login.css";
 import truck from "../../assets/truck.png";
 
