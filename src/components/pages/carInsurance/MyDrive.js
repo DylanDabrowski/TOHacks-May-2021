5caebf34b1ed1d27a4818fd426cc434ea7595c44
Dylan Dabrowski <dabrowsd@sheridancollege.ca>
2021-05-09 00:17:59 -0400

    changed f to F

--- src/components/pages/carInsurance/MyDrive.js
+++ src/components/pages/carInsurance/MyDrive.js
@@ -2,94 +2,87 @@ import "./CarInsurance.css";
 import "./MyDrive.css";
 import SideNav from "../../CarSideNav.js";
 import graphImage from "./../../../assets/graph.png";
-import mapImage from "./../../../assets/map.png"
-import db from '../../../firebase'
-import React,{useState,useEffect} from 'react';
-
-
+import mapImage from "./../../../assets/map.png";
+import db from "../../../Firebase";
+import React, { useState, useEffect } from "react";
 
 function MyDrive() {
   const [info, setInfo] = useState([]);
   const [image, setImage] = useState([]);
-const fetchInfo = async() => {
-  const response = db.collection('users');
-  const data = await response.get();
-  data.docs.forEach(item => {
-    setInfo([...info, item.data()])
-  })
-}
+  const fetchInfo = async () => {
+    const response = db.collection("users");
+    const data = await response.get();
+    data.docs.forEach((item) => {
+      setInfo([...info, item.data()]);
+    });
+  };
 
-const fetchImageInfo = async() => {
-  const imgResponse = db.storage().ref("users/" + db.auth().currentUser.uid + "/profile.png")
-  const imgData = await imgResponse.getDownloadURL()
-  imgData.then((dbImage) => {
-    setImage([...dbImage. dbImage.data()]);
-  })
-}
+  const fetchImageInfo = async () => {
+    const imgResponse = db
+      .storage()
+      .ref("users/" + db.auth().currentUser.uid + "/profile.png");
+    const imgData = await imgResponse.getDownloadURL();
+    imgData.then((dbImage) => {
+      setImage([...dbImage.dbImage.data()]);
+    });
+  };
 
-useEffect(() => {
-  fetchInfo();
-}, []);
+  useEffect(() => {
+    fetchInfo();
+  }, []);
 
-
-    return (
-      <div className="mydrive">
-        {
-          info && info.map(inf => {
-            return(
-              <div>
-            <SideNav className="navBar" />
-            <div className="row">
-              <div className="column">
-                <h2>Your Score</h2>
-                <div className="score">{inf.score}</div>
-                <img className="image" src={graphImage} alt="graph" />
+  return (
+    <div className="mydrive">
+      {info &&
+        info.map((inf) => {
+          return (
+            <div>
+              <SideNav className="navBar" />
+              <div className="row">
+                <div className="column">
+                  <h2>Your Score</h2>
+                  <div className="score">{inf.score}</div>
+                  <img className="image" src={graphImage} alt="graph" />
+                </div>
+                <div className="column">
+                  <h2>Vehicle</h2>
+                  <h1>{inf.vehicle}</h1>
+                </div>
               </div>
-              <div className="column">
-                <h2>Vehicle</h2>
-                <h1>{inf.vehicle}</h1>
+              <div className="row">
+                <div className="column">
+                  <h1>Showing: weekly results</h1>
+                  <div className="row">
+                    <div className="column">
+                      <h2>Harsh Breaking</h2>
+                      <h1 className="results-box-bad">{inf.harshNumber}</h1>
+                      <h3>{inf.harshBreaking}</h3>
+                    </div>
+                    <div className="column">
+                      <h2>Speed</h2>
+                      <h1 className="results-box-good">{inf.speedNumber}</h1>
+                      <h3>
+                        Congratulations, you have exceeded the speed limit 0
+                        times this week
+                      </h3>
+                    </div>
+                    <div className="column">
+                      <h2>Focus</h2>
+                      <h1 className="results-box-good">{inf.focusNum}</h1>
+                      <h3>{inf.focus}</h3>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div className="recent-trips">
+                <h2>Recent Trips:</h2>
+                <img className="image" src={mapImage} alt="location" />
               </div>
             </div>
-            <div className="row">
-          <div className="column">
-            <h1>Showing: weekly results</h1>
-            <div className="row">
-              <div className="column">
-                <h2>Harsh Breaking</h2>
-                <h1 className="results-box-bad">{inf.harshNumber}</h1>
-                <h3>{inf.harshBreaking}</h3>
-              </div>
-              <div className="column">
-                <h2>Speed</h2>
-                <h1 className="results-box-good">{inf.speedNumber}</h1>
-                <h3>
-                  Congratulations, you have exceeded the speed limit 0 times this
-                  week
-                </h3>
-              </div>
-              <div className="column">
-                <h2>Focus</h2>
-                <h1 className="results-box-good">{inf.focusNum}</h1>
-                <h3>{inf.focus}</h3>
-              </div>
-            </div>
-          </div>
-        </div>
-        
-        <div className="recent-trips">
-          
-          <h2>Recent Trips:</h2>
-          <img className="image" src={mapImage} alt="location" />
-        </div>
-            </div>
-            )
-          })
-        }
-
-       
-        
-      </div>
-    
+          );
+        })}
+    </div>
   );
 }
 
