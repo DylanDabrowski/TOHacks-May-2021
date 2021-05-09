2b0d148b17b74a84a991536b753a69d1460b6169
Dylan Dabrowski <dabrowsd@sheridancollege.ca>
2021-05-08 20:07:51 -0400

    finished mydrive

--- src/components/pages/carInsurance/MyDrive.js
+++ src/components/pages/carInsurance/MyDrive.js
@@ -2,25 +2,14 @@ import React from "react";
 import "./CarInsurance.css";
 import "./MyDrive.css";
 import SideNav from "../../CarSideNav.js";
-import mapImage from "./../../../assets/map.png";
-import graphImage from  "./../../../assets/graph.png";
+import mydrive from "./../../../assets/mydrive.png";
 
 function MyDrive() {
   return (
-    <div>
-      <SideNav className="navBar"/>
-
-      <div className="background">
-        
-        
-
-      </div>
-      <h1 style={{textAlign: "left", top: 53, left: 551,}}>Score</h1>
-      <img className="image" src={mapImage} alt="location"/>
-      <img className="image" src={graphImage} alt="graph"/>
+    <div className="mydrive">
+      <SideNav className="navBar" />
+      <img className="image" src={mydrive} alt="graph" />
     </div>
-    
-    
   );
 }
 
